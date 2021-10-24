d4408838b8022a7e4287b08c9c9994f8ef143b75
blanca2021 <blanca.manosalva@hotmail.com>
2021-10-24 12:37:36 -0500

    Inicializacion del proyecto

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+// 1.Modelo o Entidad
+// 2.Interface
+// 3.Repositorio
+// 4.Servicios
+// 5.Controlador o Web
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/RepositorioComputer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+/**
+ *
+ * @author blanc
+ */
+@Repository
+public class RepositorioComputer {
+    
+    @Autowired
+    private InterfaceComputer crud;
+
+    public List<Computer> getAll(){
+        return (List<Computer>) crud.findAll();
+    }
+
+    public Optional<Computer> getComputer(int id){
+        return crud.findById(id);
+    }
+
+    public Computer save(Computer computer){
+        return crud.save(computer);
+    }
+    public void delete(Computer computer){
+        crud.delete(computer);
+    }
+    
+}
